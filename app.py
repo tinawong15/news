046735bb383845e635bbeaacb1fa4952dfe5527d
tinawong15 <tinaw425@gmail.com>
2020-10-16 00:24:26 -0400

    start api work

---
+++ app.py
@@ -0,0 +1,74 @@
+import os
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+
+from util import news
+
+app = Flask(__name__)
+
+app.secret_key = os.urandom(32)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("index.html")
+
+@app.route('/entertainment', methods=['GET'])
+def entertainment():
+    # try-except block added in case the API calls fail
+    try:
+        raw_json = news.top_headlines_by_topic('entertainment')
+        titles = news.list_article_titles(raw_json)
+        urls = news.list_article_urls(raw_json)
+        descriptions = news.list_article_desc(raw_json)
+        authors = news.list_article_authors(raw_json)
+        articles = {}
+    except:
+        raw_json = {"Error pulling articles from API - possible API key error!" : ""}
+        articles = {"Error pulling articles from API - possible API key error!" : ""}
+
+    for i in range(len(titles)):
+        articles[titles[i]] = [urls[i], descriptions[i], authors[i]]
+    print(articles)
+    return render_template("entertainment.html", articles=articles)
+
+@app.route('/sports', methods=['GET'])
+def sports():
+    # try-except block added in case the API calls fail
+    try:
+        raw_json = news.top_headlines_by_topic('sports')
+        titles = news.list_article_titles(raw_json)
+        urls = news.list_article_urls(raw_json)
+        descriptions = news.list_article_desc(raw_json)
+        authors = news.list_article_authors(raw_json)
+        articles = {}
+    except:
+        raw_json = {"Error pulling articles from API - possible API key error!" : ""}
+        articles = {"Error pulling articles from API - possible API key error!" : ""}
+
+    for i in range(len(titles)):
+        articles[titles[i]] = [urls[i], descriptions[i], authors[i]]
+    print(articles)
+    return render_template("sports.html", articles=articles)
+
+@app.route('/technology', methods=['GET'])
+def technology():
+    # try-except block added in case the API calls fail
+    try:
+        raw_json = news.top_headlines_by_topic('technology')
+        titles = news.list_article_titles(raw_json)
+        urls = news.list_article_urls(raw_json)
+        descriptions = news.list_article_desc(raw_json)
+        authors = news.list_article_authors(raw_json)
+        articles = {}
+    except:
+        raw_json = {"Error pulling articles from API - possible API key error!" : ""}
+        articles = {"Error pulling articles from API - possible API key error!" : ""}
+
+    for i in range(len(titles)):
+        articles[titles[i]] = [urls[i], descriptions[i], authors[i]]
+    print(articles)
+    return render_template("technology.html", articles=articles)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ util/news.py
@@ -0,0 +1,75 @@
+import json
+import urllib.request
+
+API_LINK = 'https://newsapi.org/v2/top-headlines?'
+
+with open('data/keys.json', 'r') as f:
+    api_dict = json.load(f)
+
+API_KEY = api_dict["NEWS_API"]
+
+def top_headlines_by_topic(topic):
+    '''This function gets raw json data of top headlines by topic'''
+    url = ('https://newsapi.org/v2/top-headlines?'
+       'q=' + topic + '&'
+       'language=en&'
+       'apiKey=' + API_KEY)
+    response = urllib.request.urlopen(url)
+    return json.loads(response.read())
+
+def list_article_titles(raw_json):
+    '''This function returns a list of article titles based off of raw json data'''
+    result = []
+
+    for article in raw_json['articles']:
+        if article['title'] == None:
+            result.append('No title')
+        else:
+            result.append(article['title'])
+
+    return result
+
+def list_article_authors(raw_json):
+    '''This function returns a list of article authors based off of raw json data'''
+    result = []
+
+    for article in raw_json['articles']:
+        if article['author'] == None:
+            result.append('No author')
+        else:
+            result.append(article['author'])
+
+    return result
+
+def list_article_desc(raw_json):
+    '''This function returns a list of article descriptions based off of raw json data'''
+    result = []
+
+    for article in raw_json['articles']:
+        if article['description'] == None:
+            result.append('')
+        else:
+            result.append(article['description'])
+    return result
+
+def list_article_urls(raw_json):
+    '''This function returns a list of article urls based off of raw json data'''
+    result = []
+
+    for article in raw_json['articles']:
+        if article['url'] == None:
+            result.append('')
+        else:
+            result.append(article['url'])
+    return result
+
+def list_article_imgs(raw_json):
+    '''This function returns a list of article img urls based off of raw json data'''
+    result = []
+
+    for article in raw_json['articles']:
+        if article['urlToImage'] == None:
+            result.append('')
+        else:
+            result.append(article['urlToImage'])
+    return result
